b19b7ec35656ec237207e6c8598605d43ae0b296
Chofito <rjroblesq@gmail.com>
2019-04-04 22:12:06 -0600

    Base design...

---
+++ GithubPagesChofito/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      Coming soon
+      <fa-icon icon="rocket" size="is-large" />
+    </h2>
+    <h3 class="subtitle is-6 has-text-grey">
+      Author:
+      <a href="https://github.com/Chofito">Rodolfo Robles</a>
+    </h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
